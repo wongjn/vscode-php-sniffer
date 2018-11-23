aa62c00fd885096f50615fcdfc6329e471395080
wongjn <11310624+wongjn@users.noreply.github.com>
2018-11-23 14:30:39 +0000

    Add option on when to run the validator

--- src/validator.ts
+++ src/validator.ts
@@ -8,34 +8,82 @@ import {
   TextDocument,
   Uri,
   workspace,
+  ConfigurationChangeEvent,
+  TextDocumentChangeEvent,
 } from 'vscode';
 import { spawn } from 'child_process';
 import { PHPCSReport, PHPCSMessageType } from './phpcs-report';
+import { debounce } from 'lodash';
+
+const enum runConfig {
+  save = 'onSave',
+  type = 'onType',
+}
 
 export class Validator {
-  private diagnosticCollection?: DiagnosticCollection;
+  private diagnosticCollection: DiagnosticCollection = languages.createDiagnosticCollection();
 
   /**
    * A list of documents being parsed.
    */
   private documentQueue: Set<Uri> = new Set();
 
+  /**
+   * The active validator listener.
+   */
+  private validatorListener: Disposable | null = null;
+
   constructor(subscriptions: Disposable[]) {
-    workspace.onDidChangeConfiguration(this.refresh, this, subscriptions);
+    workspace.onDidChangeConfiguration(this.onConfigChange, this, subscriptions);
     workspace.onDidOpenTextDocument(this.validate, this, subscriptions);
     workspace.onDidCloseTextDocument(this.clearDocumentDiagnostics, this, subscriptions);
     
-    this.diagnosticCollection = languages.createDiagnosticCollection();
     this.refresh();
+    this.setValidatorListener();
   }
 
   /**
    * Dispose this object.
    */
   public dispose(): void {
-    if (this.diagnosticCollection) {
-      this.diagnosticCollection.clear();
-      this.diagnosticCollection.dispose();
+    this.diagnosticCollection.clear();
+    this.diagnosticCollection.dispose();
+  }
+
+  /**
+   * Reacts on configuration change.
+   * 
+   * @param event - The configuration change event.
+   */
+  protected onConfigChange(event: ConfigurationChangeEvent): void {
+    if (event.affectsConfiguration('phpcbf.run')) {
+      this.setValidatorListener();
+    }
+
+    this.refresh();
+  }
+
+  /**
+   * Sets the validation event listening.
+   */
+  protected setValidatorListener(): void {
+    if (this.validatorListener) {
+      this.validatorListener.dispose();
+    }
+
+    const config = workspace.getConfiguration('phpcbf');
+    const run: runConfig = config.get('run', runConfig.save);
+    const delay: number = config.get('onTypeDelay', 250);
+
+    if (run === runConfig.type as string) {
+      const debounced = debounce(
+        ({ document }: TextDocumentChangeEvent): void => { this.validate(document); },
+        delay,
+      );
+      this.validatorListener = workspace.onDidChangeTextDocument(debounced);
+    }
+    else {
+      this.validatorListener = workspace.onDidSaveTextDocument(this.validate, this);
     }
   }
 
@@ -43,6 +91,9 @@ export class Validator {
    * Refreshes validation on any open documents.
    */
   protected refresh(): void {
+    this.documentQueue.clear();
+    this.diagnosticCollection!.clear();
+
     workspace.textDocuments.forEach(this.validate, this);
   }
 
@@ -100,7 +151,7 @@ export class Validator {
         console.error(error.toString());
       }
 
-      this.diagnosticCollection!.set(document.uri, diagnostics);
+      this.diagnosticCollection.set(document.uri, diagnostics);
       this.documentQueue.delete(document.uri);
     });
   }
@@ -111,7 +162,7 @@ export class Validator {
    * @param document - The document to clear diagnostics of.
    */
   protected clearDocumentDiagnostics({ uri }: TextDocument): void {
-    this.diagnosticCollection!.delete(uri);
+    this.diagnosticCollection.delete(uri);
   }
 
 }
