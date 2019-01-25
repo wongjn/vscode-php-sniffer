9751c2b578dce4ebb345b759989f48e9f04ecf08
wongjn <11310624+wongjn@users.noreply.github.com>
2019-01-25 22:35:31 +0000

    Create basic mock project fixture

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,8 +1,23 @@
 import * as assert from 'assert';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { workspace, Uri } from 'vscode';
 
-suite("Extension Tests", function () {
-  test("Something 1", function () {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('PHP Sniffer Tests', function () {
+  const projectFolder = path.join(__dirname, 'project');
+  const fixturesSource = path.join(__dirname, '..', '..', 'src', 'test', 'fixtures');
+
+  suiteSetup(function (done) {
+    this.timeout(0);
+
+    fs.removeSync(projectFolder);
+    fs.copySync(fixturesSource, projectFolder);
+
+    workspace.updateWorkspaceFolders(0, null, { uri: Uri.file(projectFolder) });
+    workspace.onDidChangeWorkspaceFolders(done);
+  });
+
+  suiteTeardown(function () {
+    fs.removeSync(projectFolder);
   });
 });
