facf907431c2ab11c9e2b3a063bb2224a3f40299
wongjn <11310624+wongjn@users.noreply.github.com>
2019-01-26 23:04:42 +0000

    Open test project path

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,23 +1,17 @@
 import * as assert from 'assert';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { workspace, Uri } from 'vscode';
 
 suite('PHP Sniffer Tests', function () {
   const projectFolder = path.join(__dirname, 'project');
   const fixturesSource = path.join(__dirname, '..', '..', 'src', 'test', 'fixtures');
 
-  suiteSetup(function (done) {
-    this.timeout(0);
-
-    fs.removeSync(projectFolder);
-    fs.copySync(fixturesSource, projectFolder);
-
-    workspace.updateWorkspaceFolders(0, null, { uri: Uri.file(projectFolder) });
-    workspace.onDidChangeWorkspaceFolders(done);
+  suiteSetup(function () {
+    fs.emptyDirSync(projectFolder);
+    return fs.copy(fixturesSource, projectFolder);
   });
 
   suiteTeardown(function () {
-    fs.removeSync(projectFolder);
+    return fs.emptyDir(projectFolder);
   });
 });
