33a7e6ca27c1a870379fe7410077e5c7a26882e1
wongjn <11310624+wongjn@users.noreply.github.com>
2019-05-07 21:24:25 +0100

    Add CliArguments unit test

---
+++ src/test/unit/cli-arguments.test.ts
@@ -0,0 +1,31 @@
+import { CliArguments } from '../../cli-arguments';
+import { deepEqual } from 'assert';
+
+suite('CliArguments', function () {
+  test('Values passed on instantiation compile correctly', function () {
+    const args1 = new CliArguments(new Map([['a', 'b']]));
+    deepEqual(args1.getAll(), ['--a=b'], 'Single argument works');
+
+    const args2 = new CliArguments(new Map([['a', 'b'], ['c', 'd']]));
+    deepEqual(args2.getAll(), ['--a=b', '--c=d'], 'Multiple arguments work');
+  });
+
+  test('Values added after instantiation compile correctly', function () {
+    const args1 = new CliArguments()
+      .set('a', 'b')
+      .set('c', 'd');
+
+    deepEqual(args1.getAll(), ['--a=b', '--c=d']);
+
+    const args2 = new CliArguments(new Map([['a', 'b']])).set('c', 'd');
+    deepEqual(args2.getAll(), ['--a=b', '--c=d']);
+  });
+
+  test('Only values that need quotes are quoted when requested', function () {
+    const args = new CliArguments()
+      .set('a', 'no-quotes')
+      .set('b', 'needs quotes');
+
+    deepEqual(args.getAll(true), ['--a=no-quotes', '--b="needs quotes"']);
+  });
+});
