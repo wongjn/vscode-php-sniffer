2c803b0f46575d60d2352a54e86c5d380ea23f31
wongjn <11310624+wongjn@users.noreply.github.com>
2019-05-15 21:47:25 +0100

    Add function doc [skip ci]

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,15 @@
  * Contains CLI utilities.
  */
 
+/**
+ * Maps CLI argument map into a formatted array of strings.
+ *
+ * @param args
+ *   A map with string keys and values.
+ *
+ * @return
+ *   The argument pairs in --a=b format.
+ */
 export function mapToCliArgs(args: Map<string, string>, quote: boolean = false): string[] {
   return Array.from(args.entries())
     .filter(([key, value]) => value !== '' && key !== '')
