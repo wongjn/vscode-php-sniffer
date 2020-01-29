538c5e012ed9fc9548aad4049be7d702f3816dc7
wongjn <11310624+wongjn@users.noreply.github.com>
2020-01-29 15:12:10 +0000

    Restructure runner test suites

--- test/integration/runner.test.js
+++ test/integration/runner.test.js
@@ -17,164 +17,175 @@ const onDidChangeWorkspaceFoldersPromise = () => new Promise((resolve) => {
 });
 
 suite('Runner', function () {
+  const mainFolder = Uri.file(path.join(__dirname, '/fixtures/config'));
+  const a = Uri.file(path.join(__dirname, '/fixtures/config/a'));
+
   suite('createRunner()', function () {
-    const mainFolder = Uri.file(path.join(__dirname, '/fixtures/config'));
+    suite('Basic functionality', function () {
+      suiteSetup(async function () {
+        // Set up workspace folder.
+        const onChange = onDidChangeWorkspaceFoldersPromise(workspace);
+        workspace.updateWorkspaceFolders(0, 0, { uri: mainFolder });
+        await onChange;
 
-    /** @type {import('vscode').Uri} */
-    let a;
+        // Get config.
+        const aConfig = workspace.getConfiguration('phpSniffer', a);
 
-    suiteSetup(async function () {
-      // Set up workspace folder.
-      const onChange = onDidChangeWorkspaceFoldersPromise();
-      workspace.updateWorkspaceFolders(0, 0, { uri: mainFolder });
-      await onChange;
-
-      // Get URIs for our PHP document fixtures.
-      [a] = (await workspace.findFiles('**/a', undefined, 1));
-
-      // Get config.
-      const aConfig = workspace.getConfiguration('phpSniffer', a);
-
-      // Set workspace config.
-      await Promise.all([
-        aConfig.update('standard', 'A-Standard', ConfigurationTarget.Workspace),
-        aConfig.update('executablesFolder', '../execs/', ConfigurationTarget.Workspace),
-        aConfig.update('snippetExcludeSniffs', ['ExcludeMe', 'IgnoreThis'], ConfigurationTarget.Workspace),
-      ]);
-    });
-
-    suiteTeardown(async function () {
-      const aConfig = workspace.getConfiguration('phpSniffer', a);
-
-      // Revert configuration.
-      await Promise.all([
-        aConfig.update('standard', undefined, ConfigurationTarget.Workspace),
-        aConfig.update('executablesFolder', undefined, ConfigurationTarget.Workspace),
-        aConfig.update('snippetExcludeSniffs', undefined, ConfigurationTarget.Workspace),
-      ]);
-
-      // Revert workspace folders.
-      const onRevert = onDidChangeWorkspaceFoldersPromise();
-      workspace.updateWorkspaceFolders(0, 1);
-      return onRevert;
-    });
-
-    suite('.phpcs()', function () {
-      test('Binary file location', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        assert.doesNotReject(run.phpcs('a'));
+        // Set workspace config.
+        await Promise.all([
+          aConfig.update('standard', 'A-Standard', ConfigurationTarget.Workspace),
+          aConfig.update('executablesFolder', '../execs/', ConfigurationTarget.Workspace),
+          aConfig.update('snippetExcludeSniffs', ['ExcludeMe', 'IgnoreThis'], ConfigurationTarget.Workspace),
+        ]);
       });
 
-      test('Returns JSON-parsed STDOUT', async function () {
-        const tokenSource = new CancellationTokenSource();
-        const run = createRunner(tokenSource.token, a);
+      suiteTeardown(async function () {
+        const aConfig = workspace.getConfiguration('phpSniffer', a);
 
-        assert.strictEqual(String(await run.phpcs('a')), '[object Object]');
+        // Revert configuration.
+        await Promise.all([
+          aConfig.update('standard', undefined, ConfigurationTarget.Workspace),
+          aConfig.update('executablesFolder', undefined, ConfigurationTarget.Workspace),
+          aConfig.update('snippetExcludeSniffs', undefined, ConfigurationTarget.Workspace),
+        ]);
+
+        // Revert workspace folders.
+        const onRevert = onDidChangeWorkspaceFoldersPromise(workspace);
+        workspace.updateWorkspaceFolders(0, 1);
+        return onRevert;
       });
 
-      test('Token cancellation returns null', async function () {
-        const tokenSource = new CancellationTokenSource();
-        const run = createRunner(tokenSource.token, a);
+      suite('.phpcs()', function () {
+        test('Binary file location', function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          return assert.doesNotReject(run.phpcs('a'));
+        });
 
-        const result = run.phpcs('a');
-        setTimeout(() => tokenSource.cancel(), 1);
+        test('Returns JSON-parsed STDOUT', async function () {
+          const tokenSource = new CancellationTokenSource();
+          const run = createRunner(tokenSource.token, a);
 
-        assert.strictEqual(await result, null);
+          assert.strictEqual(String(await run.phpcs('a')), '[object Object]');
+        });
+
+        test('Token cancellation returns null', async function () {
+          const tokenSource = new CancellationTokenSource();
+          const run = createRunner(tokenSource.token, a);
+
+          const result = run.phpcs('a');
+          setTimeout(() => tokenSource.cancel(), 1);
+
+          assert.strictEqual(await result, null);
+        });
+
+        test('Function parameter passed through as STDIN', async function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          const stdin = String(Math.random());
+
+          assert.strictEqual((await run.phpcs(stdin)).stdin, stdin);
+        });
+
+        test('Args are passed through', async function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          const result = await run.phpcs('a');
+
+          assert(result.arg.includes(` --stdin-path=${a.fsPath} `));
+          assert(result.arg.includes(' --report=json '));
+          assert(result.arg.includes(' --standard=A-Standard '));
+          assert(result.arg.includes(' --runtime-set ignore_warnings_on_exit 1 '));
+          assert(result.arg.includes(' --runtime-set ignore_errors_on_exit 1 '));
+          assert(result.arg.includes(' -q '));
+          assert(result.arg.endsWith(' -'));
+        });
       });
 
-      test('Function parameter passed through as STDIN', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        const stdin = String(Math.random());
+      suite('.phpcbf()', function () {
+        test('Binary file location', function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          return assert.doesNotReject(run.phpcbf('a'));
+        });
 
-        assert.strictEqual((await run.phpcs(stdin)).stdin, stdin);
-      });
+        test('Returns raw STDOUT string', async function () {
+          const tokenSource = new CancellationTokenSource();
+          const run = createRunner(tokenSource.token, a);
 
-      test('Args are passed through', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        const result = await run.phpcs('a');
+          assert.strictEqual(typeof await run.phpcbf('a'), 'string');
+        });
 
-        assert(result.arg.includes(` --stdin-path=${a.fsPath} `));
-        assert(result.arg.includes(' --report=json '));
-        assert(result.arg.includes(' --standard=A-Standard '));
-        assert(result.arg.includes(' --runtime-set ignore_warnings_on_exit 1 '));
-        assert(result.arg.includes(' --runtime-set ignore_errors_on_exit 1 '));
-        assert(result.arg.includes(' -q '));
-        assert(result.arg.endsWith(' -'));
-      });
-    });
+        test('Token cancellation returns original text', async function () {
+          const tokenSource = new CancellationTokenSource();
+          const run = createRunner(tokenSource.token, a);
+          const stdin = String(Math.random());
 
-    suite('.phpcbf()', function () {
-      test('Binary file location', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        assert.doesNotReject(run.phpcbf('a'));
-      });
+          const result = run.phpcbf(stdin);
+          setTimeout(() => tokenSource.cancel(), 1);
 
-      test('Returns raw STDOUT string', async function () {
-        const tokenSource = new CancellationTokenSource();
-        const run = createRunner(tokenSource.token, a);
+          assert.strictEqual(await result, stdin);
+        });
 
-        assert.strictEqual(typeof await run.phpcbf('a'), 'string');
-      });
+        test('Function parameter passed through as STDIN', async function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          const stdin = String(Math.random());
+          const result = JSON.parse(await run.phpcbf(stdin));
 
-      test('Token cancellation returns original text', async function () {
-        const tokenSource = new CancellationTokenSource();
-        const run = createRunner(tokenSource.token, a);
-        const stdin = String(Math.random());
+          assert.strictEqual(result.stdin, stdin);
+        });
 
-        const result = run.phpcbf(stdin);
-        setTimeout(() => tokenSource.cancel(), 1);
+        test('Args are passed through', async function () {
+          const run = createRunner(new CancellationTokenSource().token, a);
+          const result = JSON.parse(await run.phpcbf('a'));
 
-        assert.strictEqual(await result, stdin);
-      });
+          assert(result.arg.includes(` --stdin-path=${a.fsPath} `));
+          assert(result.arg.includes(' --standard=A-Standard '));
+          assert(result.arg.endsWith(' -'));
+        });
 
-      test('Function parameter passed through as STDIN', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        const stdin = String(Math.random());
-        const result = JSON.parse(await run.phpcbf(stdin));
+        test('Sniffs are excluded for partial document', async function () {
+          const run = createRunner(new CancellationTokenSource().token, a, false);
+          const result = JSON.parse(await run.phpcbf('a'));
 
-        assert.strictEqual(result.stdin, stdin);
-      });
-
-      test('Args are passed through', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a);
-        const result = JSON.parse(await run.phpcbf('a'));
-
-        assert(result.arg.includes(` --stdin-path=${a.fsPath} `));
-        assert(result.arg.includes(' --standard=A-Standard '));
-        assert(result.arg.endsWith(' -'));
-      });
-
-      test('Sniffs are excluded for partial document', async function () {
-        const run = createRunner(new CancellationTokenSource().token, a, false);
-        const result = JSON.parse(await run.phpcbf('a'));
-
-        assert(result.arg.includes(' --exclude=ExcludeMe,IgnoreThis '));
+          assert(result.arg.includes(' --exclude=ExcludeMe,IgnoreThis '));
+        });
       });
     });
 
     suite('Folder resolution', function () {
       const subfolder = Uri.file(path.join(__dirname, '/fixtures/config/subfolder'));
       const secondaryFolder = Uri.file(path.join(__dirname, '/fixtures/config0'));
-
-      /** @type {import('vscode').Uri} */
-      let b;
-      /** @type {import('vscode').Uri} */
-      let c;
+      const b = Uri.file(path.join(__dirname, '/fixtures/config/subfolder/b'));
+      const c = Uri.file(path.join(__dirname, '/fixtures/config0/c'));
 
       suiteSetup(async function () {
         // Set up additional workspace folders.
-        const onChange = onDidChangeWorkspaceFoldersPromise();
-        workspace.updateWorkspaceFolders(1, 0, { uri: subfolder }, { uri: secondaryFolder });
+        const onChange = onDidChangeWorkspaceFoldersPromise(workspace);
+        workspace.updateWorkspaceFolders(
+          0,
+          0,
+          { uri: mainFolder },
+          { uri: subfolder },
+          { uri: secondaryFolder },
+        );
         await onChange;
 
-        // Get URIs for our PHP document fixtures.
-        [b, c] = (await workspace.findFiles('**/{b,c}', undefined, 2));
+        // Set workspace config.
+        const config = workspace.getConfiguration('phpSniffer', a);
+        await Promise.all([
+          config.update('standard', 'A-Standard', ConfigurationTarget.Workspace),
+          config.update('executablesFolder', '../execs/', ConfigurationTarget.Workspace),
+        ]);
       });
 
       suiteTeardown(async function () {
+        // Set workspace config.
+        const config = workspace.getConfiguration('phpSniffer', a);
+        await Promise.all([
+          config.update('standard', undefined, ConfigurationTarget.Workspace),
+          config.update('executablesFolder', undefined, ConfigurationTarget.Workspace),
+        ]);
+
         // Revert workspace folders.
-        const onRevert = onDidChangeWorkspaceFoldersPromise();
-        workspace.updateWorkspaceFolders(1, 2);
+        const onRevert = onDidChangeWorkspaceFoldersPromise(workspace);
+        workspace.updateWorkspaceFolders(0, 3);
         return onRevert;
       });
 
