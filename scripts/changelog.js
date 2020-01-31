aea2a87f6f4b7be0cf122f6a243f34fa2db3f66a
wongjn <11310624+wongjn@users.noreply.github.com>
2020-01-31 22:37:48 +0000

    Write changelog script [ci skip]

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,14 +1,20 @@
 /**
  * @file
  * Modifies CHANGELOG.md during npm-version command.
- *
- * @todo Rewrite for updated CHANGELOG format.
  */
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// @ts-nocheck
 
-rl.question('Update changelog', () => rl.close());
+const fs = require('fs').promises;
+const path = require('path');
+const { version } = require('../package.json');
+
+const filePath = path.resolve(__dirname, '../CHANGELOG.md');
+
+fs.readFile(filePath, 'utf8')
+  .then(
+    (content) => content
+      .replace(/(## \[Unreleased\])/, `$1\n\n## [${version}] - ${new Date().toISOString().slice(0, 10)}`)
+      .replace(/(\[Unreleased\]:) (.+)(\d+\.\d+\.\d+)(.+)/, `$1 $2${version}$4\n[${version}]: $2$3...v${version}`),
+  )
+  .then((content) => fs.writeFile(filePath, content));
